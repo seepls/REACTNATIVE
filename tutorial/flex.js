9d0eb4da16b550cd97a52da72efdb09349508ed1
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-12-01 22:34:02 +0530

    Create flex.js

---
+++ tutorial/flex.js
@@ -0,0 +1,22 @@
+//basic flex :
+
+import React ,{Component} from 'react ;
+import {Text ,View} from ' react-native';
+export default class myApp extends Component {
+ render(){
+  return(
+    <View style = {{ flex: 1 }}>
+      <View style = {{ flex:1 ,backgroundColor: 'green'}}
+         <Text style={{color: "white"}}> white color 
+         </Text>
+      </View>
+      .
+      .
+      .
+    </View>
+    );
+    }
+    }
+ 
+ 
+
