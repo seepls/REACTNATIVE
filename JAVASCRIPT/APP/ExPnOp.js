0e87a7d7e1f889373b177d9759d1438b459dd41b
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-12-15 16:12:44 +0530

    Create ExPnOp.js

---
+++ JAVASCRIPT/APP/ExPnOp.js
@@ -0,0 +1,67 @@
+/*
+following kind of operators : 
+1. Assignment : similar to c 
+2. comparision  : compares the operand and returns a logic value 
+when comparing operators not of the same kind , JS converts them '
+!= ( not equal ) == ( equal)  checking 
+=== ( strictly equal) 
+!== : striclty not equal 
+3 .arithmetics operators :
+ same except for  (**) = exponential operator 
+4.unary operator : 
+ (i) : delete : 
+   delete objectName ; 
+   delete objectName.property;
+   delete objectName[index] ; 
+   delete property ; // used to delete within a with statement 
+   
+   delete operator if succeds set the property or element to undefined 
+   delete statement. in array : still holds the rest of the indexes values as it is , just returns undefiend value which ever index you delete 
+   
+   (ii) typeof operator  : 
+          
+             typeof operand
+             typeof(operand ) 
+    eg : 
+    typeof true ; // returns boolean 
+    typeof null ; // returns object 
+    
+   (iii) void operator : 
+       void( expression)
+       void expression
+       
+       // specifies an expression to be evaluated without returning a value .
+     expression is a JS : expression to evaluate 
+     
+   (iv) in operator :
+       returns true if specified property is in the specified object 
+       syntax : 
+        propNameOrNumber in objectName 
+    (v) instanceof : returns true if object is of specified object isof the specied object type
+        syntax : objectName  instanceof objectType
+        
+        used to confirm type of object at runtime 
+        
+ExPression::: 
+
+
+primary expression 
+this : 
+refer to current object. this refers to calling object in method 
+new : 
+create instance of user-defined object type or of one of the built in object types 
+
+syntax : 
+var objectName = new objectType([param1 , param 2 ..... paraml])
+
+super : 
+used to call functions on an object's parent 
+useful with classes to call parent constructor 
+
+super([arguments]);
+super.functionOnPrent([argument]);
+
+
+
+
+  
